1ff427b2aaaf03a56584b7a6a06575b9b70b1ba9
zacwoll@github.com <zacwoll@github.com>
2020-07-21 20:28:16 +0000

    made algo more efficient

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,23 +1,28 @@
 #include "binary_trees.h"
 
-int is_ancestor(const binary_tree_t *search, const binary_tree_t *key)
+int is_ancestor(const binary_tree_t *search, const binary_tree_t *key,
+		const binary_tree_t *prev)
 {
-	if (!search)
+	/* this algorithm is inefficient, one idea is to add a 'seen' value */
+	if (!search || search == prev)
 		return (0);
 	if (search == key)
 		return (1);
-	return (ancestor(search->left, key) | ancestor(search->right, key));
+	return (is_ancestor(search->left, key, prev) |
+			is_ancestor(search->right, key, prev));
 }
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
 	binary_tree_t *search = (binary_tree_t *)first;
+	binary_tree_t *prev = NULL;
 
 	while (search)
 	{
-		if (is_ancestor(search, second))
+		if (is_ancestor(search, second, prev))
 			return (search);
+		prev = search;
 		search = search->parent;
 	}
 
