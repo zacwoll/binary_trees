b009649baca8f7218cd862febb113737d2ede5bf
zacwoll@github.com <zacwoll@github.com>
2020-07-24 18:41:43 +0000

    didn't include 114

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * bst_search - searches a binary search tree for a value
+ * @tree: binary tree to be searched
+ * @value: key to search for
+ * Return: bst_t *node or NULLL
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (!tree)
+		return (NULL);
+	if (value > tree->n)
+		return (bst_search(tree->right, value));
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	return ((bst_t *)tree);
+}
+
+/**
  * in_order_successor - finds the next in-order successor to a binary tree node
  * @root: root of binary tree to search
  * Return: bst_t *successor or NULL
